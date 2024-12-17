aaa0cc21c1e16aeb7ba36fa742e497bab46e7d25
Ayah Darwish <ayah.darwish@tktk.ee>
2024-12-17 22:17:52 +0200

    add week6 code

---
+++ RandomElement/Program.cs
@@ -0,0 +1,7 @@
+﻿string[] snacks = { "rice", "sushi", "wok noodles", "Eastern food", "chicken wings" };
+
+Random rnd = new Random();
+
+int randomIndex  = rnd.Next(0, snacks.Length);
+
+Console.WriteLine($"Tonight we are going to eat {snacks[randomIndex]}.");
